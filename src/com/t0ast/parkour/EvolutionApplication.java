11036d7dd43636a6a235a0989dabafc35ec247e3
T0astBread <m.briedl@live.at>
2017-05-27 19:33:35 +0200

    Some Visualization
    There is now some nice visualization going on here. However the actual training environment is still a bit buggy.

--- src/com/t0ast/parkour/EvolutionApplication.java
+++ src/com/t0ast/parkour/EvolutionApplication.java
@@ -7,7 +7,6 @@ package com.t0ast.parkour;
 
 import com.badlogic.gdx.math.Polygon;
 import com.t0ast.evolution.EvolvingPool;
-import com.t0ast.evolution.entities.Entity;
 import com.t0ast.parkour.training.ParkourEnvironment;
 import com.t0ast.evolution.entities.instructional.InstructionGenerator;
 import com.t0ast.evolution.entities.instructional.InstructionalEntityGenerator;
@@ -15,12 +14,12 @@ import com.t0ast.evolution.entities.instructional.InstructionalMutator;
 import com.t0ast.evolution.misc.selectors.EqualRandomListElementSelector;
 import com.t0ast.evolution.misc.selectors.FirstListElementSelector;
 import com.t0ast.evolution.training.trainers.CumulativeTrainer;
-import com.t0ast.evolution.training.trainers.IndividualTrainer;
 import com.t0ast.parkour.instructions.MoveInstruction;
 import com.t0ast.parkour.instructions.TurnInstruction;
 import com.t0ast.parkour.training.ParkourEntity;
 import com.t0ast.parkour.training.ParkourFitnessRater;
 import com.t0ast.parkour.training.ParkourResults;
+import com.t0ast.parkour.visualization.VisualizationPanel;
 
 /**
  *
@@ -29,34 +28,51 @@ import com.t0ast.parkour.training.ParkourResults;
 public class EvolutionApplication
 {
 
-    public EvolutionApplication()
+    private VisualizationPanel visualization;
+    private EvolvingPool<ParkourEntity, ParkourResults> pool;
+    private ParkourEnvironment environment;
+
+    public EvolutionApplication(VisualizationPanel visualizationPanel)
     {
-        ParkourEnvironment env = getTrainingEnv();
+        this.visualization = visualizationPanel;
+        this.environment = getTrainingEnv();
+        this.environment.setVisualization(this.visualization);
         ParkourFitnessRater rater = new ParkourFitnessRater();
         InstructionGenerator ig = new InstructionGenerator();
         ig.registerInstruction(MoveInstruction.class);
         ig.registerInstruction(TurnInstruction.class);
-        EvolvingPool<ParkourEntity, ParkourResults> pool = new EvolvingPool<>(new CumulativeTrainer<>(env, rater),
+        this.pool = new EvolvingPool<>(new CumulativeTrainer<>(this.environment, rater),
         100, 50, new FirstListElementSelector(), new EqualRandomListElementSelector(),
         new InstructionalMutator<>(ig, new EqualRandomListElementSelector()), EvolvingPool.MutationType.SINGLE_PARENT,
         new InstructionalEntityGenerator<>(new ParkourEntity(), ig));
-        pool.initialize();
-        for(int i = 0; i < 10000; i++)
+        this.pool.initialize();
+        if(visualizationPanel != null)
         {
-            pool.nextGen();
+            visualizationPanel.drawEnvironment(this.environment, this.pool.getEntities());
         }
-        pool.getEntities().stream().map(Entity::getFitness).forEach(System.out::println);
     }
 
     private ParkourEnvironment getTrainingEnv()
     {
-        return new ParkourEnvironment(300, 100,
-        new Polygon(new float[]
+        Polygon obst = new Polygon();
+        obst.setPosition(100, 50);
+        obst.setOrigin(100, 50);
+        obst.setVertices(new float[]
         {
-            100, 50,
-            100, 95,
-            250, 70,
-            220, 50
-        }));
+            0, 0,
+            0, 45,
+            150, 20,
+            120, 0
+        });
+        return new ParkourEnvironment(300, 100, obst);
+    }
+
+    public void doGenerations(int generations)
+    {
+        for(int i = 0; i < generations; i++)
+        {
+            this.pool.nextGen();
+        }
+        this.visualization.drawEnvironment(this.environment, this.pool.getEntities());
     }
 }
--- src/com/t0ast/parkour/training/ParkourEnvironment.java
+++ src/com/t0ast/parkour/training/ParkourEnvironment.java
@@ -9,11 +9,11 @@ import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
-import com.t0ast.evolution.entities.Entity;
 import com.t0ast.evolution.entities.instructional.instructions.Instruction;
 import com.t0ast.evolution.training.trainers.environments.CumulativeTrainingEnvironment;
 import com.t0ast.parkour.instructions.MoveInstruction;
 import com.t0ast.parkour.instructions.TurnInstruction;
+import com.t0ast.parkour.visualization.VisualizationPanel;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,6 +28,7 @@ public class ParkourEnvironment implements CumulativeTrainingEnvironment<Parkour
 
     private int width, height;
     private Polygon[] obstacles;
+    private VisualizationPanel visualization;
 
     public ParkourEnvironment(int width, int height, Polygon... obstacles)
     {
@@ -52,32 +53,40 @@ public class ParkourEnvironment implements CumulativeTrainingEnvironment<Parkour
 
         for(int i = 0; i < MAX_CYCLES_PER_TRAINING; i++)
         {
+            int stillHaveInstructionsLeft = 0;
             for(ParkourEntity entity : entities)
             {
-                updateCycle(entity, i);
+                if(updateCycle(entity, i)) stillHaveInstructionsLeft++;
+            }
+            if(stillHaveInstructionsLeft == 0) break;
+            if(this.visualization != null)
+            {
+                this.visualization.drawEnvironmentUpdateStep(this, entities);
             }
         }
-        
+
         return entities.stream().map(this::createResults).toArray(ParkourResults[]::new);
     }
-    
+
     private ParkourResults createResults(ParkourEntity entity)
     {
-        return new ParkourResults((Math.round(entity.getPosition().x) * 1f)/this.width, entity);
+        return new ParkourResults((Math.round(entity.getPosition().x) * 1f) / this.width, entity);
     }
 
-    private void updateCycle(ParkourEntity entity, final int cycle)
+    private boolean updateCycle(ParkourEntity entity, final int cycle)
     {
         if(cycle >= entity.getInstructionSize())
         {
-            return;
+            return false;
         }
         Instruction currentInst = entity.getInstructions().get(cycle);
         updateCycle(entity, currentInst);
+        return true;
     }
 
     private void updateCycle(ParkourEntity entity, Instruction currentInst)
     {
+        entity.getPosition().set(0, 0);
         if(currentInst instanceof MoveInstruction)
         {
             move(entity, ((MoveInstruction) currentInst).getValue());
@@ -97,11 +106,11 @@ public class ParkourEnvironment implements CumulativeTrainingEnvironment<Parkour
         {
             if(Intersector.intersectLinePolygon(entity.getPosition(), finalPosition, o, finalPosition))
             {
-                finalPosition.add(entity.getPosition());
+                finalPosition.sub(entity.getPosition());
             }
         });
 
-        entity.getPosition().set(finalPosition);
+        entity.getPosition().add(finalPosition);
         entity.getPosition().x = MathUtils.clamp(entity.getPosition().x, 0, this.width);
         entity.getPosition().y = MathUtils.clamp(entity.getPosition().y, 0, this.height);
     }
@@ -109,5 +118,25 @@ public class ParkourEnvironment implements CumulativeTrainingEnvironment<Parkour
     private void turn(ParkourEntity entity, int amountDeg)
     {
         entity.getDirection().setAngle(amountDeg);
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public Polygon[] getObstacles()
+    {
+        return obstacles;
+    }
+
+    public void setVisualization(VisualizationPanel visualization)
+    {
+        this.visualization = visualization;
     }
 }
